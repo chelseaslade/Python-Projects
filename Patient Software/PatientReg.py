c2b293b0dbe515576651515461e3b1dabebbe7a5
chelseaslade <153530277+chelseaslade@users.noreply.github.com>
2024-02-16 20:33:09 -0330

    Commit PatientReg.py

---
+++ Patient Software/PatientReg.py
@@ -0,0 +1,43 @@
+#Patient Registration Software Dev
+#Start Date: February 16
+#Author: Chelsea Slade
+
+#Import Libraries
+import datetime
+import sqlite3
+
+connection = sqlite3.connect("patientRecords.db")
+
+#Constants
+
+#Main Program Loop
+continueProg = True
+while continueProg == True: 
+
+#User Inputs
+  print()
+  ptFirstName = input("Enter patients first name: ").title()
+  ptLastName = input("Enter patients last name: ").title()
+  ptDOB = input("Enter the patients date of birth (YEAR/MONTH/DAY, ex. 2000/01/30): ")
+  ptMCP = input("Enter the patients MCP number: ")
+  ptStAddress = input("Enter the patients street address: (Ex. 99 Hospital Road) ").title()
+  ptCity = input("Enter the patients city: ").title()
+  ptPostalCode = input("Enter the patients postal code: ").upper()
+  ptFamDoc = input("Enter the patients family doctor: ").title()
+  print()
+
+#Calculations
+
+#Patient ID #: 
+
+#Output
+  print()
+  print(f"Patient Name: {ptFirstName} {ptLastName} ")
+  print(f"Patient Date of Birth: {ptDOB} ")
+  print(f"Patient Address: {ptStAddress} {ptCity} {ptPostalCode} ")
+  print(f"Patient MCP #: {ptMCP} ")
+  print(f"Patient Family Doctor: {ptFamDoc} ")
+  print()
+
+
+#Housekeeping
