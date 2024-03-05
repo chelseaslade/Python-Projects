644810a822178367aea0276050972f5f067c9541
chelseaslade <153530277+chelseaslade@users.noreply.github.com>
2024-03-05 13:25:40 -0330

    Database Connection

--- Patient Software/PatientReg.py
+++ Patient Software/PatientReg.py
@@ -6,8 +6,65 @@
 #Import Libraries
 import datetime
 import sqlite3
+from sqlite3 import Error
 
-connection = sqlite3.connect("patientRecords.db")
+#SQL Lite 3 Database
+
+
+def create_connection(patientRecords):
+  # connection = sqlite3.connect("patientRecords.db")
+  conn = None
+  try:
+    conn = sqlite3.connect(patientRecords)
+    return conn
+  except Error as x: 
+    print(x)
+  # finally:
+  #   if conn:
+  #     conn.close()
+
+  return conn
+
+def create_table(conn, create_table_sql):
+  try:
+    c = conn.cursor()
+    c.execute(create_table_sql)
+  except Error as x:
+    print(x)
+
+def main():
+  
+  database = "patientRecords.db"
+
+  sql_create_projects_table = """ CREATE TABLE projects (
+                                        id integer PRIMARY KEY,
+                                        name text NOT NULL,
+                                        begin_date text,
+                                        end_date text
+                                    ); """
+
+  sql_create_tasks_table = """ CREATE TABLE tasks (
+                                    id integer PRIMARY KEY,
+                                    name text NOT NULL,
+                                    priority integer,
+                                    status_id integer NOT NULL,
+                                    project_id integer NOT NULL,
+                                    begin_date text NOT NULL,
+                                    end_date text NOT NULL,
+                                    FOREIGN KEY (project_id) REFERENCES projects (id)
+                              ); """
+
+  conn = create_connection("patientRecord=")
+  if conn is not None: 
+    create_table(conn, sql_create_projects_table)
+    create_table(conn, sql_create_tasks_table)
+    print("Success, tables created")
+
+  else:
+    print("Error! Cannot create database connection.")
+               
+if __name__ == '__main__':
+    main()
 
 #Constants
 
