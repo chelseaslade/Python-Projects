5f057745a701cae028df82a341b23e5fabf3a67b
chelseaslade <153530277+chelseaslade@users.noreply.github.com>
2024-03-11 13:31:38 -0230

    Added Future Options
    To add: Add table, delete data, rename, etc.

--- Patient Software/databaseEdits.py
+++ Patient Software/databaseEdits.py
@@ -1,5 +1,7 @@
 import sqlite3
 
+#Create Menu for Selection (TBD)
+
 con = sqlite3.connect("/Users/student/Documents/GitHub - Stored Projects/Python-Projects/Patient Software/patientRecords.db")
 cur = con.cursor()
 print("Program has connected to the Patient Records database.")
@@ -10,10 +12,16 @@ result = cur.fetchall()
 print("SQLite version is {}".format(result))
 print("--------------------------------------------")
 
-#DATABASE EDITS
+#Add Column to Table
 
-addColumn = "ALTER TABLE patient ADD COLUMN ptFamDoc varchar(32)"
-cur.execute(addColumn)
+# addColumn = "ALTER TABLE patient ADD COLUMN ptFamDoc varchar(32)"
+# cur.execute(addColumn)
+
+#Add Table to Database
+
+#Delete Data
+
+#Rename 
 
 con.commit()
 con.close()
