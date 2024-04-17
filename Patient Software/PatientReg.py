29a89af6197cabc171a391d62e5f970250581ae6
chelseaslade <153530277+chelseaslade@users.noreply.github.com>
2024-04-17 20:38:48 -0230

    Add Patient Function - skeleton

--- Patient Software/PatientReg.py
+++ Patient Software/PatientReg.py
@@ -7,8 +7,7 @@
 import datetime
 import sqlite3
 from sqlite3 import Error
-
-#Constants
+import patientRegFunctions as PF
 
 
 #Welcome to the Program Message
@@ -19,131 +18,20 @@ print("--------------------------------------------")
 
 #SQL Lite 3 Database
 try: 
-  con = sqlite3.connect("/Users/student/Documents/GitHub - Stored Projects/Python-Projects/Patient Software/patientRecords.db")
-  cur = con.cursor()
-  print("Program has connected to the Patient Records database.")
-  query = "select sqlite_version();"
-  cur.execute(query)
+   con = sqlite3.connect("/Users/student/Documents/GitHub - Stored Projects/Python-Projects/Patient Software/patientRecords.db")
+   cur = con.cursor()
+   print("Program has connected to the Patient Records database.")
+   query = "select sqlite_version();"
+   cur.execute(query)
 
-  result = cur.fetchall()
-  print("SQLite version is {}".format(result))
-  print("--------------------------------------------")
+   result = cur.fetchall()
+   print("SQLite version is {}".format(result))
+   print("--------------------------------------------")
 
-
-  #Main Program Loop
-  continueProg = True
-  while continueProg == True: 
-
-  #User Inputs
-    
-    allowed_characters = set("abcdefghijklmnopqrstuvwxyz ABCEDFGHIJKLMNOPQRSTUVWXYZ ,.-'")
-    allowed_num_only = set("1234567890")
-    allowed_num= set("1234567890-., ")
-
-    print("To exit the program, type END in first name input.")
-    print("Enter patient information as indicated below: ")
-    print()
-
-    while True:
-      ptFirstName = input("Enter patients first name: ").title()
-      if ptFirstName.upper() == "END":
-         continueProg = False
-         break
-      elif ptFirstName == "":
-         print("Data entry error: Patient first name cannot be blank. Please re-enter.") 
-         continue
-      else:
-         break
-      
-    if continueProg == False:
-       break
-    
-    while True:   
-      ptLastName = input("Enter patients last name: ").title()
-      if ptLastName == "":
-         print("Data entry error: Patient last name cannot be blank. Please re-enter.")
-         continue
-      else: 
-         break
-        
-    while True:
-      ptDOB = input("Enter the patients date of birth (YEAR/MONTH/DAY, ex. 2000/01/30): ")
-      if ptDOB == "":
-         print("Data entry error: Patient date of birth cannot be blank. Please re-enter.")
-         continue
-      else:
-         break
-
-    while True:
-      ptMCP = input("Enter the patients MCP number: ")
-      if ptMCP == "":
-         print("Data entry error: Patient MCP cannot be blank. Please re-enter.")
-         continue
-      else:
-         break
-      
-
-    while True:
-      ptStAddress = input("Enter the patients street address: (Ex. 99 Hospital Road) ").title()
-      if ptStAddress == "":
-         print("Data entry error: Patient street address cannot be blank. Please re-enter.")
-         continue
-      else:
-         break
-
-    while True: 
-      ptCity = input("Enter the patients city: ").title()
-      if ptCity == "":
-         print("Data entry error: Patient city cannot be blank. Please re-enter.")
-         continue
-      else:
-         break
-    
-    while True:
-      ptPostalCode = input("Enter the patients postal code: ").upper()
-      if ptPostalCode == "":
-         print("Data entry error: Patient postal code cannot be blank. Please re-enter.")
-         continue
-      else:
-         break
-   
-    while True:
-      provLst = ["AB", "BC", "NB", "NS", "NL", "QC", "ON", "YT", "NT", "NU", "MB", "SK", "PE"]
-      ptProvince = input("Enter the patients province: (ie. XX) ").upper()
-      if ptProvince == "":
-         print("Data entry error: Patient postal code cannot be blank. Please re-enter.")
-         continue
-      else:
-         break
-
-    while True:
-      ptFamDoc = input("Enter the patients family doctor: ").title()
-      if ptFamDoc == "":
-         print("Data entry error: Patient family doctor cannot be blank. Please re-enter. If the patient has no family doctor at this time, please enter 'none'.")
-         continue
-      else:
-         break
-    
-  #Calculations
-    
-  #Patient ID #: 
-
-  #Output
-    print("--------------------------------------------")
-    print()
-    print("PATIENT INFORMATION SUMMARY")
-    print()
-    print(f"Patient Name: {ptFirstName} {ptLastName} ")
-    print(f"Patient Date of Birth: {ptDOB} ")
-    print(f"Patient Address: {ptStAddress} {ptCity} {ptPostalCode} ")
-    print(f"Patient MCP #: {ptMCP} ")
-    print(f"Patient Family Doctor: {ptFamDoc} ")
-    print()
-    print("--------------------------------------------")
-
+   #Menu Option One: Add Patient
+   PF.addPatient()
 
 #Housekeeping
-  
 except sqlite3.Error as error:
    print("Error occured -", error)
 
@@ -154,5 +42,5 @@ finally:
       print("--------------------------------------------")
       print("******* SQLite Connection Closed *******")
 
-print("Thank you for using the Patient Registration Program!")
-print("--------------------------------------------")
+   print("Thank you for using the Patient Registration Program!")
+   print("--------------------------------------------")
---
+++ Patient Software/patientRegFunctions.py
@@ -0,0 +1,110 @@
+#Functions for Patient Reg Project
+#Author: Chelsea Slade
+#Date: April 17, 2024 -
+
+#Add Patient (Option 1)
+allowed_characters = set("abcdefghijklmnopqrstuvwxyz ABCEDFGHIJKLMNOPQRSTUVWXYZ ,.-'")
+allowed_num_only = set("1234567890")
+allowed_num= set("1234567890-., ")
+
+print("To exit the program, type END in first name input.")
+print("Enter patient information as indicated below: ")
+print()
+
+#Main Program Loop
+def addPatient():
+    continueProg = True
+    while continueProg == True: 
+
+        while True:
+            ptFirstName = input("Enter patients first name: ").title()
+            if ptFirstName.upper() == "END":
+                continueProg = False
+                break
+            elif ptFirstName == "":
+                print("Data entry error: Patient first name cannot be blank. Please re-enter.") 
+                continue
+            else:
+                break
+            
+        if continueProg == False:
+            break
+
+        while True:   
+            ptLastName = input("Enter patients last name: ").title()
+            if ptLastName == "":
+                print("Data entry error: Patient last name cannot be blank. Please re-enter.")
+                continue
+            else: 
+                break
+            
+        while True:
+            ptDOB = input("Enter the patients date of birth (YEAR/MONTH/DAY, ex. 2000/01/30): ")
+            if ptDOB == "":
+                print("Data entry error: Patient date of birth cannot be blank. Please re-enter.")
+                continue
+            else:
+                break
+
+        while True:
+            ptMCP = input("Enter the patients MCP number: ")
+            if ptMCP == "":
+                print("Data entry error: Patient MCP cannot be blank. Please re-enter.")
+                continue
+            else:
+                break
+            
+
+        while True:
+            ptStAddress = input("Enter the patients street address: (Ex. 99 Hospital Road) ").title()
+            if ptStAddress == "":
+                print("Data entry error: Patient street address cannot be blank. Please re-enter.")
+                continue
+            else:
+                break
+
+        while True: 
+            ptCity = input("Enter the patients city: ").title()
+            if ptCity == "":
+                print("Data entry error: Patient city cannot be blank. Please re-enter.")
+                continue
+            else:
+                break
+
+        while True:
+            ptPostalCode = input("Enter the patients postal code: ").upper()
+            if ptPostalCode == "":
+                print("Data entry error: Patient postal code cannot be blank. Please re-enter.")
+                continue
+            else:
+                break
+
+        while True:
+            provLst = ["AB", "BC", "NB", "NS", "NL", "QC", "ON", "YT", "NT", "NU", "MB", "SK", "PE"]
+            ptProvince = input("Enter the patients province: (ie. XX) ").upper()
+            if ptProvince == "":
+                print("Data entry error: Patient postal code cannot be blank. Please re-enter.")
+                continue
+            else:
+                break
+
+        while True:
+            ptFamDoc = input("Enter the patients family doctor: ").title()
+            if ptFamDoc == "":
+                print("Data entry error: Patient family doctor cannot be blank. Please re-enter. If the patient has no family doctor at this time, please enter 'none'.")
+                continue
+            else:
+                break
+        
+    if continueProg == True:
+        print("--------------------------------------------")
+        print()
+        print("PATIENT INFORMATION SUMMARY")
+        print()
+        print(f"Patient Name: {ptFirstName} {ptLastName} ")
+        print(f"Patient Date of Birth: {ptDOB} ")
+        print(f"Patient Address: {ptStAddress} {ptCity} {ptPostalCode} ")
+        print(f"Patient MCP #: {ptMCP} ")
+        print(f"Patient Family Doctor: {ptFamDoc} ")
+        print()
+        print("--------------------------------------------")
